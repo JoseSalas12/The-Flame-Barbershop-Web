8e337a1a07522d569d84c99f1e734d9f5baec9a6
Jose Salas <josesalasev@gmail.com>
2025-04-18 10:56:57 -0500

    Astro landing Deploy

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+
+// https://astro.build/config
+export default defineConfig({
+  experimental: {
+      svg: {
+          mode: 'sprite',
+      },
+  },
+  site: 'https://JoseSalas12.github.io',
+  base: 'The-Flame-Barbershop-Web',
+
+
+});
